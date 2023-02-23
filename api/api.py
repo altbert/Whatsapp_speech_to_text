61138eeee6db3840d91896902e4f80f9da458685
altbert <joaquinaltamirano@protonmail.com>
2023-02-23 18:41:00 +0100

    Second commit

---
+++ api/api.py
@@ -0,0 +1,39 @@
+from flask import Flask, abort, request
+import whisper
+from tempfile import NamedTemporaryFile
+
+# Load the Whisper model:
+model = whisper.load_model('base')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def handler():
+    if not model:
+        model = whisper.load_model(request_model)
+        
+    if not request.files:
+        # If the user didn't submit any files, return a 400 (Bad Request) error.
+        abort(400)
+
+    # For each file, let's store the results in a list of dictionaries.
+    results = []
+
+    # Loop over every file that the user submitted.
+    for filename, handle in request.files.items():
+        # Create a temporary file.
+        # The location of the temporary file is available in `temp.name`.
+        temp = NamedTemporaryFile()
+        # Write the user's uploaded file to the temporary file.
+        # The file will get deleted when it drops out of scope.
+        handle.save(temp)
+        # Let's get the transcript of the temporary file.
+        result = model.transcribe(temp.name)
+        # Now we can store the result object for this file.
+        results.append({
+            'filename': filename,
+            'transcript': result['text'],
+        })
+
+    # This will be automatically converted to JSON.
+    return {'results': results}
