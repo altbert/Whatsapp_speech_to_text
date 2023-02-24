ec2daf4001c48933f4cfe8e0b875c22c55fc404a
altbert <joaquinaltamirano@protonmail.com>
2023-02-24 20:23:26 +0100

    index.js cleaned a, commented and added env variables for compatibility | README.md Updated | Dockerfile edcomented unused models

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Speech to text in Whatsapp using [Whisper](https://github.com/openai/whisper) an
 ### Usage
 - To build the images run ```docker-compose build```
 - To run the containers run ```docker-compose up``` (Do not dettach, the qr will me displayed in the terminal)
+
+### Configuration
+- Edit the model you need in the api file located in api/api.py ```model = whisper.load_model('YOU_MODEL')```. Default model: **tiny**
+- Uncoment the model you want to use and comment the rest in the Dockerfile located in api/Dockerfile. Default model: **tiny**
+- To configure the path and the api address edit the enviroment variables inside the ```docker-compose.yml``` file. The default values are: 
+  - HOST_ADDRESS=whisper_api
+  - CHROME_DATA_PATH="/app/data/"
+- If you want to use the code outside docker, you just have to edit the env variables in the index.js file.
--- node/index.js
+++ node/index.js
@@ -1,107 +1,42 @@
 const fs = require('fs');
+
+// Required for Terminal QRCode
 const qrcode = require('qrcode-terminal');
 
+// Required for POST request to api
 const request = require('request');
 
+// Required for Whatsapp Web connection
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
+// Required for ENV Setup
+const process = require('node:process');
 
+// Setup env variables so it can run on docker and also as standalone
+if (process.env.HOST_ADDRESS && process.env.CHROME_DATA_PATH) {
+	apiHost = process.env.HOST_ADDRESS;
+	dataPath = process.env.CHROME_DATA_PATH;
+} else {
+	apiHost = "127.0.0.1";
+	dataPath = "./"
+}
+
+// Setup options for the client and data path for the google chrome session
 const client = new Client({
-	authStrategy: new LocalAuth({ dataPath: "/app/data/" }),
-	// authStrategy: new LocalAuth(),
+	authStrategy: new LocalAuth({ dataPath: dataPath }),
 	puppeteer: {
 		headless: true,
 		args: ['--no-sandbox', '--disable-setuid-sandbox']
 	}
 });
 
-
+// Initialize client
 client.initialize();
 
+// Generates a qr in the console (for authentication)
 client.on('qr', qr => {
 	qrcode.generate(qr, {small: true});
 });
-
-//Lista blanca de contactos. Si el contacto esta agendado entonces el script funciona
-async function ContactsWhiteList(Contact) {
-	let ContactInfo = await client.getContactById(Contact);
-	Contact = ContactInfo.name
-
-	if (ContactInfo.isMyContact) {
-		return [Contact, 1];
-	} else {
-		return [Contact, 0];
-	}
-}
-
-//Genera una fecha y una hora basado en el timestamp del mensaje (unix time)
-function GetDate(timestamp) {
-		//https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-		var date = new Date(timestamp * 1000);
-		var year = date.getFullYear();
-		var month = date.getMonth();
-		var day = date.getDate();
-		var hours = date.getHours();
-		var minutes = "0" + date.getMinutes();
-		var seconds = "0" + date.getSeconds();
-
-		var formattedDate = day+"-"+month+"-"+year;
-		var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-		return [formattedTime, formattedDate];
-}
-
-//Mensajes automatizados
-async function AutomatedMessages(message) {
-
-	let chat = await message.getChat();
-
-	if(message.body == '!tran' && message.hasQuotedMsg){
-		const quotedMsgTemp = await message.getQuotedMessage();
-		let quotedMsg = new Message(client, {
-			id: { _serialized: quotedMsgTemp.id },
-			hasMedia: true, // --> IMPORTANT
-		  });
-		
-		  const media = await quotedMsg.downloadMedia();
-		
-		// console.log("Text: "+quotedMsg.body);
-		message.reply(quotedMsg.body);
-		// client.sendMessage(message.from, 'test');
-	}
-}
-
-//Text to speech function
-async function SpeechToTextTranscript(base64data, message) {
-	const decodedBuffer = Buffer.from(base64data, 'base64');
-
-	// Send the decoded binary file to the Flask API
-	request.post({
-		url: 'http://172.25.0.2:5000',
-		// url: 'http://127.0.0.1:5000',
-		formData: {
-		file: {
-		  value: decodedBuffer,
-		  options: {
-			filename: message.from + message.timestamp
-		  }
-		}
-	  }
-	}, function(err, httpResponse, body) {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log('Upload successful! Server responded with:', body);
-			
-			const data = JSON.parse(body);
-			for (const result of data.results) {
-				const transcript = result.transcript;
-				console.log(transcript);
-				message.reply("Esto es una transcripcion automatica del audio:\n\n"+transcript);
-			}
-		}
-	});
-}
 
 //Log successful client connection
 client.on('ready', () => {
@@ -133,14 +68,86 @@ client.on('message', async message => {
 				(await chat).markUnread();
 			}
 		}
-		//TODO: Convinar esta funcion con la de arriba
-		//https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
-		console.log('\x1b[32m%s:\x1b[0m %s \x1b[5m%s\x1b[0m', Contact, message_text, formattedTime);
-		//console.log(message) //debug
+
+		console.log('\x1b[32m%s:\x1b[0m %s \x1b[5m%s\x1b[0m', Contact, message.type, formattedTime);
 
 	}
 });
 
-//Useful links
-//https://stackoverflow.com/questions/2917175/return-multiple-values-in-javascript
-//https://stackoverflow.com/questions/40999025/javascript-scope-variable-to-switch-case
+// Contact white list. If the sender is your contact, the audio file will be transcript
+async function ContactsWhiteList(Contact) {
+	let ContactInfo = await client.getContactById(Contact);
+	Contact = ContactInfo.name
+
+	if (ContactInfo.isMyContact) {
+		return [Contact, 1];
+	} else {
+		return [Contact, 0];
+	}
+}
+
+// Date and hour based on the timestamp of the mesage (unix time)
+function GetDate(timestamp) {
+		var date = new Date(timestamp * 1000);
+		var year = date.getFullYear();
+		var month = date.getMonth();
+		var day = date.getDate();
+		var hours = date.getHours();
+		var minutes = "0" + date.getMinutes();
+		var seconds = "0" + date.getSeconds();
+
+		var formattedDate = day+"-"+month+"-"+year;
+		var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+		return [formattedTime, formattedDate];
+}
+
+// TODO: when replyed with !tran, the worker will transcribe only the audio quoted
+async function AutomatedMessages(message) {
+
+	let chat = await message.getChat();
+
+	if(message.body == '!tran' && message.hasQuotedMsg){
+		const quotedMsgTemp = await message.getQuotedMessage();
+		let quotedMsg = new Message(client, {
+			id: { _serialized: quotedMsgTemp.id },
+			hasMedia: true, // --> IMPORTANT
+		});
+		
+		const media = await quotedMsg.downloadMedia();
+		message.reply(quotedMsg.body);
+	
+	}
+}
+
+// Text to speech function
+// TODO: reply to message outside this function
+async function SpeechToTextTranscript(base64data, message) {
+	const decodedBuffer = Buffer.from(base64data, 'base64');
+
+	// Send the decoded binary file to the Flask API
+	request.post({
+		url: 'http://'+ apiHost +':5000',
+		formData: {
+		file: {
+		  value: decodedBuffer,
+		  options: {
+			filename: message.from + message.timestamp
+		  }
+		}
+	  }
+	}, function(err, httpResponse, body) {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log('Upload successful! Server responded with:', body);
+			
+			const data = JSON.parse(body);
+			for (const result of data.results) {
+				const transcript = result.transcript;
+				console.log(transcript);
+				message.reply("Esto es una transcripcion automatica del audio:\n\n"+transcript);
+			}
+		}
+	});
+}
